caffeae38622027a1c924c50f9fe6fe1b6de1ce3
Brian Cruz <cruz.b.james@gmail.com>
2021-09-23 19:30:33 -0500

    Map

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
 const colors = ['teal', 'magenta', 'cyan']
 function myForEach(arr, func) {
     for (el of arr) {
-        func(el);
+        func(el, arr.indexOf(el), arr);
     }
 }
-myForEach(colors, function (color) {
-    console.log(color.toUpperCase());
+myForEach(colors, function (color, el) {
+    console.log(color.toUpperCase(), 'at index of ', el);
+})
+
+//map
+
+const todos = [
+    {
+        id: '1',
+        text: 'Groceries',
+        priority: 'High'
+    },
+    {
+        id: '2',
+        text: 'Play CoD',
+        priority: 'Low'
+    },
+    {
+        id: '3',
+        text: 'Grade CodeHS',
+        priority: 'High'
+    },
+    {
+        id: '4',
+        text: 'Progress Monitor',
+        priority: 'Medium'
+    }
+];
+
+const todosText = todos.map(function (todo) {
+    return todo.text;
+});
+
+const links = Array.from(document.querySelectorAll('a'));
+
+const urls = links.map(function (a) {
+    return a.href;
 })
